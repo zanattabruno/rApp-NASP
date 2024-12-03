cf72323f315c75b8f79a2c500d23b2aa4ebc56a2
zanattabruno <gkbsa2504@gmail.com>
2024-12-03 01:29:09 -0300

    Add policy data extraction and PRB calculation to create_app; refactor tools.py

--- src/rApp_NASP.py
+++ src/rApp_NASP.py
@@ -5,8 +5,7 @@ import requests
 import yaml
 import uuid
 import sys
-from tools import extract_flow_bit_rate, to_prb
-
+from tools import extract_policy_data
 from flask import Flask, request, jsonify
 from rApp_catalogue_client import rAppCatalogueClient
 
@@ -203,12 +202,9 @@ def create_app(config, logger):
 
         data = request.get_json()
         logger.debug(f"Received data: {json.dumps(data, indent=2)}")
+        policy_data= extract_policy_data(data)
+        logger.debug(f"Extracted data: {json.dumps(policy_data, indent=2)}")
 
-        downlink_min, downlink_max = extract_flow_bit_rate(data)
-        logger.debug(f"Downlink min: {downlink_min}, Downlink max: {downlink_max}")
-        downlink_min_prb = to_prb(downlink_min, False, 28, 1, 50, is_tdd=False)
-        downlink_max_prb = to_prb(downlink_max, False, 28, 1, 50, is_tdd=False)
-        logger.debug(f"Downlink min PRB: {downlink_min_prb}, Downlink max PRB: {downlink_max_prb}")
         # Validate input data
         if not isinstance(data, list):
             logger.error("Invalid data format. Expected a list of E2 nodes.")
--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,59 @@
 import json
 
+def extract_policy_data(json_data):
+    downlink_min = None
+    downlink_max = None
+    mcc = None
+    mnc = None
+    sst = None
+    sd = None
+
+    def search(data):
+        nonlocal downlink_min, downlink_max, mcc, mnc, sst, sd
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == "Guaranteed Flow Bit Rate - Downlink":
+                    downlink_min = value
+                elif key == "Max Flow Bit Rate - Downlink":
+                    downlink_max = value
+                elif key == "mcc":
+                    mcc = str(value).zfill(3)
+                elif key == "mnc":
+                    mnc = str(value).zfill(2)
+                elif key == "sst":
+                    sst = value
+                elif key == "sd":
+                    sd = value
+                else:
+                    search(value)
+        elif isinstance(data, list):
+            for item in data:
+                search(item)
+
+    search(json_data)
+    
+    minPRB = to_prb(downlink_min, False, 28, 1, 50, is_tdd=False)
+    maxPRB = to_prb(downlink_max, False, 28, 1, 50, is_tdd=False)
+
+
+    # Construct the output JSON
+    output = {
+        "RRMPolicyRatioList": [
+            {
+                "plmnId": {
+                    "mcc": mcc if mcc else "001",
+                    "mnc": mnc if mnc else "01"
+                },
+                "sst": sst if sst else 1,
+                "sd": sd if sd else 1,
+                "minPRB": minPRB,
+                "maxPRB": maxPRB
+            }
+        ]
+    }
+
+    return output
+
 def extract_flow_bit_rate(json_data):
     downlink_min = None
     downlink_max = None
