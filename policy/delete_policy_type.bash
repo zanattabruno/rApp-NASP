2db58fa101b3d6e83253cdca74a651c10da0cf1d
zanattabruno <gkbsa2504@gmail.com>
2024-12-16 22:49:00 -0300

    Update delete_policy_type.bash to change POLICY_ID and modify curl command endpoint

--- policy/delete_policy_type.bash
+++ policy/delete_policy_type.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Define the policy ID to delete
-POLICY_ID=4
+POLICY_ID=1
 
 # Execute the curl command to delete the policy
-curl -v -X DELETE http://service-ricplt-a1mediator-http.ricplt.svc.cluster.local:10000/a1-p/policytypes/${POLICY_ID}
+#curl -v -X DELETE http://service-ricplt-a1mediator-http.ricplt.svc.cluster.local:10000/a1-p/policytypes/${POLICY_ID}
+
+curl -v -X DELETE http://10.101.111.161:10000/a1-p/policytypes/${POLICY_ID}
